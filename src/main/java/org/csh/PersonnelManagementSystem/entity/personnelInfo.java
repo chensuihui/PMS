3dc64936bd96c0b2b406377c307a03406bf08c3f
csh <2870986806@qq.com>
2021-04-02 09:51:09 +0800

    base

---
+++ src/main/java/org/csh/PersonnelManagementSystem/entity/personnelInfo.java
@@ -0,0 +1,122 @@
+package org.csh.PersonnelManagementSystem.entity;
+
+import java.util.Date;
+
+public class personnelInfo {
+    /*
+    员工编号，主键
+     */
+    private Integer personnelID;
+    /*
+    员工姓名
+     */
+    private String personnelName;
+    /*
+    性别
+     */
+    private String personnelSex;
+    /*
+    出生日期
+     */
+    private Date birthday;
+   /*
+    身份证号
+     */
+    private Integer ID;
+    /*
+    婚姻状况
+     */
+    private String wedlock;
+    /*
+    名族
+     */
+    private String race;
+    /*
+    籍贯
+     */
+    private String nativePlace;
+    /*
+    政治面貌
+     */
+    private String politic;
+    /*
+    电子邮箱
+     */
+    private String E_mail;
+    /*
+    联系电话
+     */
+    private Integer phone;
+    /*
+    联系地址
+     */
+    private String address;
+    /*
+    部门
+     */
+    private String department;
+    /*
+    职位
+     */
+    private String job;
+    /*
+    职称
+     */
+    private String duty;
+    /*
+    基本工资
+     */
+    private Double basisSalary;
+    /*
+    聘用形式
+     */
+    private String engageForm;
+    /*
+    最高学历
+     */
+    private String TiptopDegree;
+    /*
+    所属专业
+     */
+    private String specialty;
+    /*
+    毕业院校
+     */
+    private String school;
+    /*
+    入职日期
+     */
+    private Date beginWorkDate;
+    /*
+    在职状态
+     */
+    private String workState;
+    /*
+    工号
+     */
+    private Integer workID;
+    /*
+    合同期限
+     */
+    private Integer contractTerm;
+    /*
+    转正日期
+     */
+    private Date beFormDate;
+    /*
+    离职日期
+     */
+    private Date notWorkState;
+    /*
+    合同起始日期
+     */
+    private Date beginContract;
+    /*
+    合同终止日期
+     */
+    private Date endContract;
+    /*
+    工龄
+     */
+    private Integer workAge;
+}
